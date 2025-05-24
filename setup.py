30778561705668ffc786dc1c5b3263dc106e0a78
Avinash <avimallick@gmail.com>
2025-05-25 00:57:37 +0100

    Updated readme and version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="topoindex",
-    version="0.1.5",
+    version="0.1.6",
     packages=find_packages(),
     install_requires=["networkx", "rdkit", "pandas"],
     author="Avinash Mallick",
